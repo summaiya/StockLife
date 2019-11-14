e949c91af10b9434cdddffeb296d673d91e6816a
Your Name <you@example.com>
2019-11-14 12:51:51 -0800

    CRUD update

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const tourContent = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours
 const app = express();
 app.use(express.json())//Simple Middleware<--- It parsing json obj
 
+//FIND Item
+const findItem = (items, itemFind)=>{
+    return items.find(element=>element.id===itemFind)
+}
 //GET
 app.get('/api/v1/tours', (req, res)=>{
     res.status(200).json(
@@ -21,7 +25,8 @@ app.get('/api/v1/tours', (req, res)=>{
 
 //GET Single Pack
 app.get('/api/v1/tours/:id', (req, res)=>{
-    const ans = tourContent.find(element=> element.id === JSON.parse(req.params.id));
+    //const ans = tourContent.find(element=> element.id === JSON.parse(req.params.id));
+    const ans = findItem(tourContent, JSON.parse(req.params.id))
     if(ans === undefined){
         res.status(404).send({
             status: 'failed',
@@ -57,8 +62,8 @@ app.post('/api/v1/tours', (req, res)=>{
 })
 //UPDATE (PUT, PATCH)
 app.patch(`/api/v1/tours/:id`, (req, res)=>{
-    const currentPack = tourContent.find(element=>JSON.parse(req.params.id)===element.id);
-    
+    //const currentPack = tourContent.find(element=>JSON.parse(req.params.id)===element.id);
+    const currentPack = findItem(tourContent, JSON.parse(req.params.id));
     if(currentPack !== undefined){
         const inputData = req.body;
         const newCurrentPack = Object.assign(currentPack, inputData);
@@ -81,9 +86,31 @@ app.patch(`/api/v1/tours/:id`, (req, res)=>{
             message: 'NOT FOUND ITEM'
         })
     }
-
 })
 
+//Delete
+app.delete('/api/v1/tours/:id', (req, res)=>{
+    const deletePack = findItem(tourContent, JSON.parse(req.params.id));
+    console.log('deletePack', deletePack)
+   if(deletePack !== undefined){
+        tourContent.splice(tourContent.indexOf(deletePack), 1);
+        fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tourContent), err=>{
+            console.log(`delete error ${err}`)
+        })
+        res.status(200).json({
+            status: 'success',
+            length: tourContent.length,
+            data: tourContent
+        })
+   }
+   else{
+    res.status(404).json({
+        status: 'failed',
+        message: 'NOT FOUND ITEM'
+    })
+   }
+    
+})
 const PORT = 3000;
 app.listen(PORT, ()=>{
     console.log(`Server is Starting on ${PORT}`)
