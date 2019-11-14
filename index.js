457f021bfcf3c48c10a681563d6dec57413c069c
Your Name <you@example.com>
2019-11-13 18:53:59 -0800

    update

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const fs = require('fs');
 
+//Get json file
+const tourContent = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 const app = express();
+app.use(express.json())//Simple Middleware
+
+//GET
+app.get('/api/v1/tours', (req, res)=>{
+    res.status(200).json(
+        {
+            status: "success",
+            dataLength: tourContent.length,
+            data: {
+                tourContent
+            }
+        }
+    );
+})
+//POST
+app.post('/api/v1/tours', (req, res)=>{
+    console.log(req.body);  
+    res.send('Your Information Has Saved');
+})
+
 
 const PORT = 3000;
 app.listen(PORT, ()=>{
     console.log(`Server is Starting on ${PORT}`)
 })
+
