13c3d2ed58deef3623a67ea7282c6db7652b11fa
eric-programming <eric09279@outlook.com>
2019-12-01 00:01:01 -0800

    updateReviewModel

--- Model/reviewModeling.js
+++ Model/reviewModeling.js
@@ -8,6 +8,10 @@ const reviewSchema = new mongoose.Schema({
         max: [5, "Maximum Rating is 5.0"],
         min: [0, "Minimum Rating is 0"]
     },
+    review: {
+        type: String,
+        require: [true, "Review must not be empty!"]
+    },
     createAt: {
         type: Date,
         default: Date.now()
